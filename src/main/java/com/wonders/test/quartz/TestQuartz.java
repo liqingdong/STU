d4b0f3cc3747eec6438d0d1d8c57d487d9522b91
liqingdong <229436569@qq.com>
2017-11-06 23:32:39 +0800

    update

--- src/main/java/com/wonders/test/quartz/TestQuartz.java
+++ src/main/java/com/wonders/test/quartz/TestQuartz.java
@@ -11,7 +11,7 @@ public class TestQuartz {
 
     public void execute() {
         Date date = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd  HH:mm:ss");
-        System.out.println("定时任务开始执行,现在时间是："+sdf.format(date));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("定时任务开始执行,现在时间是：" + sdf.format(date));
     }
 }
